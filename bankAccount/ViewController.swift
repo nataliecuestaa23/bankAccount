37ba1dd0bce40b7e89adf0fe5b63c80ac5afc6b5
nataliescuestaa23 <cuestanatalie@gmail.com>
2019-07-15 13:20:20 -0700

    Intial Commit

---
+++ bankAccount/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  bankAccount
+//
+//  Created by Apple on 7/15/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+  
+    //get user and pass text
+    
+  
+    
+    @IBOutlet weak var inputUser: UITextField!
+    @IBOutlet weak var inputPassword: UITextField!
+    //press button, check if correct
+  
+    @IBAction func userCheck(_ sender: Any) {
+    }
+        if inputUser.text == "testUser" {
+        if inputPassword.text == "testPass"{
+        
+                //need to link second page's label here
+                //then display "you are correct" in label
+            }
+        } else {
+            //label says "you are wrong"
+        }
+        }
+            }
+    
+
+
+
+
